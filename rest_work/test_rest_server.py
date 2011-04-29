887f5c465045ed9b7371dc8357e3b732d1542378
Michael Ricordeau <m.ricordeau@newtech.fr>
2011-04-29 17:07:23 +0200

    add logger with config

--- rest_work/test_rest_server.py
+++ rest_work/test_rest_server.py
@@ -1,3 +1,4 @@
+from plivo.utils.logger import StdoutLogger, FileLogger, SysLogger
 import os
 from gevent.wsgi import WSGIServer
 from flask import Flask
@@ -16,6 +17,8 @@ class RestServer(InboundEventSocket, RestApi):
         self.app = Flask(self.name)
         # load config into flask app
         self.app.config.from_envvar('PLIVO_REST_CONFIG')
+        # init logger
+        self._init_logger()
         # create rest server
         fs_host, fs_port = self.app.config['FS_INBOUND_ADDRESS'].split(':', 1)
         fs_port = int(fs_port)
@@ -29,11 +32,30 @@ class RestServer(InboundEventSocket, RestApi):
         # create wsgi server
         http_host, http_port = self.app.config['HTTP_ADDRESS'].split(':', 1)
         http_port = int(http_port)
-        self.http_server = WSGIServer((http_host, http_port), self.app)
+        self.http_server = WSGIServer((http_host, http_port), self.app, log=self.logger)
+
+    def _init_logger(self):
+        logdebug = self.app.config.get('DEBUG', False)
+        logtype = self.app.config.get('LOG_TYPE', 'stdout')
+        if logtype == 'syslog':
+            syslog_addr = self.app.config.get('SYSLOG_ADDRESS', '/dev/log')
+            syslog_facility = self.app.config.get('SYSLOG_FACILITY', 'local0')
+            self.logger = SysLogger(syslog_addr, syslog_facility)
+        elif logtype == 'file':
+            logfile = self.app.config.get('LOG_FILE', '/tmp/ivr.log')
+            self.logger = FileLogger(logfile)
+        else:
+            self.logger = StdoutLogger()
+        self.logger.name = self.name
+        self.app._logger = self.logger
+        if logdebug:
+            self.logger.set_debug()
+        else:
+            self.logger.set_info()
 
     def start(self):
         # run
-        self.app.debug = True
+        #self.app.debug = True
         #self.app.run()
         #self.connect()
         self.http_server.serve_forever()
