fbde767a80e590e1651f214194df2d19a5b5c82f
Michael Ricordeau <m.ricordeau@newtech.fr>
2011-04-29 11:54:02 +0200

    get config using flask config

--- rest_work/test_rest_server.py
+++ rest_work/test_rest_server.py
@@ -1,7 +1,7 @@
+import os
 from gevent.wsgi import WSGIServer
 from flask import Flask
 from plivo.core.freeswitch.inboundsocket import InboundEventSocket
-from config import Config
 from api import RestApi
 import urls
 
@@ -11,23 +11,22 @@ class RestServer(InboundEventSocket, RestApi):
     name = "RestServer"
 
     def __init__(self, configfile):
+        os.environ['PLIVO_REST_CONFIG'] = configfile
         # create flask app
         self.app = Flask(self.name)
-        # create and load config
-        self.config = Config(configfile)
         # load config into flask app
-        self.app.config.from_object(self.config)
+        self.app.config.from_envvar('PLIVO_REST_CONFIG')
         # create rest server
-        fs_host, fs_port = self.config['FS_INBOUND_ADDRESS'].split(':', 1)
+        fs_host, fs_port = self.app.config['FS_INBOUND_ADDRESS'].split(':', 1)
         fs_port = int(fs_port)
-        fs_password = self.config['FS_PASSWORD']
+        fs_password = self.app.config['FS_PASSWORD']
         InboundEventSocket.__init__(self, fs_host, fs_port, fs_password, filter='ALL')
         # expose api functions to flask app
         for path, func in urls.URLS.iteritems():
             fn = getattr(self, func.__name__)
             self.app.add_url_rule(path, func.__name__, fn)
         # create wsgi server
-        http_host, http_port = self.config['HTTP_ADDRESS'].split(':', 1)
+        http_host, http_port = self.app.config['HTTP_ADDRESS'].split(':', 1)
         http_port = int(http_port)
         self.http_server = WSGIServer((http_host, http_port), self.app)
 
